a29abfe7e28fd48f82091c8a1af42384f5ae60e0
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-15 23:29:55 +0600

    restaurant model defined

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -7,4 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'description', 'address', 'phone', 'logo', 'cover', 'enable_ordering', 'stripe_id', 'user_id'];
+
+    // define accessors
+    public function getLogoAttribute($value) {
+        return $value ? asset('storage/' . $value) : null;
+    }
+
+    public function getCoverAttribute($value) {
+        return $value ? asset('storage/' . $value) : null;
+    }
+
+    // define relationships
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories() {
+        return $this->hasMany(Category::class);
+    }
+
+    public function dineinTables() {
+        return $this->hasMany(DineinTable::class);
+    }
 }
