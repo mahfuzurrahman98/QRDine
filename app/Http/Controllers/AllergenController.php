6df839bd4afd7a8a47465aa892ace8818f3e0e85
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-15 23:34:15 +0600

    allergen controller initialized

--- app/Http/Controllers/AllergenController.php
+++ app/Http/Controllers/AllergenController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AllergenController extends Controller
-{
-    //
+class AllergenController extends Controller {
+    public function index() {
+        return view('allergens.index');
+    }
 }
