3659e7281f6b4feb1ea491b7c7d286d015131a9e
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-15 23:27:36 +0600

    dinein table model defined

--- app/Models/DineinTable.php
+++ app/Models/DineinTable.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class DineinTable extends Model
-{
+class DineinTable extends Model {
     use HasFactory;
+
+    protected $fillable = ['name', 'restaurant_id', 'active'];
+
+    // define relationships
+    public function restaurant() {
+        return $this->belongsTo(Restaurant::class);
+    }
 }
