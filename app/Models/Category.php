536f876d1d33c6d2c65232674f46eac99106245d
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-15 23:27:24 +0600

    category model defined

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
-{
+class Category extends Model {
     use HasFactory;
+
+    protected $fillable = ['name', 'restaurant_id', 'active', 'order_index'];
+
+    // define relationships
+    public function restaurant() {
+        return $this->belongsTo(Restaurant::class);
+    }
+
+    public function items() {
+        return $this->hasMany(Item::class);
+    }
 }
