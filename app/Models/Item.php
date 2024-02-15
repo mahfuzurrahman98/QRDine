62cc6519e3dd7108e0676eb67771329aa14b4e92
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-15 23:29:42 +0600

    item model defined

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Item extends Model
-{
+class Item extends Model {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'image', 'active', 'category_id'];
+
+    // define accessors
+    public function getImageAttribute($value) {
+        return $value ? asset('storage/' . $value) : null;
+    }
+
+    // define relationships
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function allergens() {
+        return Allergen::findByItem($this->id);
+    }
 }
