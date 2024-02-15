220a581c6a5a49ee4aeedf03bdbcbc7aaebb7578
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-15 23:33:30 +0600

    all routes are defined

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/', function () {
 
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'authenticate']);
+Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
 
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'store']);
