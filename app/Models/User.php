14685b7eaace682784c3edb287939c3afec1116f
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-16 18:18:00 +0600

    added relationship with restaurant

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable {
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // define relationship
+    public function restaurant() {
+        return $this->hasOne(Restaurant::class);
+    }
 }
