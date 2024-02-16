26e7b9fdcbd71510907fa790155121ceefac0e91
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-17 00:13:25 +0600

    disabled attribute added

--- resources/views/components/form/input-text.blade.php
+++ resources/views/components/form/input-text.blade.php
@@ -1,8 +1,9 @@
 <div>
-    <label for="name" class="block mb-2 text-sm font-medium text-white">
+    <label for="{{ $id }}" class="block mb-2 text-sm font-medium text-white">
         {{ $label }}
     </label>
     <input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}"
-        class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
-        value="{{ $value }}" placeholder="{{ $placeholder }}" required="{{ $required }}" />
+        class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white {{ $disabled ? 'opacity-75' : '' }}"
+        value="{{ $value }}" placeholder="{{ $placeholder }}" required="{{ $required }}"
+        {{ $disabled ? 'disabled' : '' }} />
 </div>
