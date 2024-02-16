a87c201ce238465014a76f18a0736e1252344a57
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-17 00:12:43 +0600

    index page returned on function call

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class OrderController extends Controller
-{
-    //
+class OrderController extends Controller {
+    public function index() {
+        return view('orders.index');
+    }
 }
