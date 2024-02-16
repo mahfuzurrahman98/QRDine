4b50f6dc464db1b49ce315c3baf07575f55cb083
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-17 00:13:08 +0600

    placement change for attribute

--- app/View/Components/Form/InputText.php
+++ app/View/Components/Form/InputText.php
@@ -7,32 +7,35 @@ use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class InputText extends Component {
+    public string $label;
     public string $type;
     public string $name;
     public string $id;
     public string $value;
     public string $placeholder;
     public bool $required;
-    public string $label;
+    public bool $disabled;
     /**
      * Create a new component instance.
      */
     public function __construct(
+        string $label,
         string $type,
         string $name,
         string $id,
         string $value,
         string $placeholder,
         bool $required,
-        string $label
+        bool $disabled = false
     ) {
+        $this->label = $label;
         $this->type = $type;
         $this->name = $name;
         $this->id = $id;
         $this->value = $value;
         $this->placeholder = $placeholder;
         $this->required = $required;
-        $this->label = $label;
+        $this->disabled = $disabled;
     }
 
     /**
