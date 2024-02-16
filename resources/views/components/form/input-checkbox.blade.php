2440ea52009d62bfc0cc9ee8638d3440e0d4deac
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-16 22:17:37 +0600

    checkbox component added

---
+++ resources/views/components/form/input-checkbox.blade.php
@@ -0,0 +1,11 @@
+<div class="flex items-start">
+    <div class="flex items-center h-5">
+        <input id="{{ $id }}" name="{{ $name }}" type="checkbox" required="{{ $required }}"
+            class="w-4 h-4 rounded bg-gray-700 border-gray-600" checked="{{ $checked }}">
+    </div>
+    <div class="ml-3 text-sm">
+        <label for="{{ $id }}" class="font-medium text-white">
+            {{ $label }}
+        </label>
+    </div>
+</div>
