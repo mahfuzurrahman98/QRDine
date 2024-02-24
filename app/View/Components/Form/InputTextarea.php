8ce46e473d103e2a312a83715468991db302c442
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-24 13:11:05 +0600

    added disabled attribute to input textarea component

--- app/View/Components/Form/InputTextarea.php
+++ app/View/Components/Form/InputTextarea.php
@@ -7,30 +7,33 @@ use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class InputTextarea extends Component {
+    public string $label;
     public string $name;
     public string $id;
     public string $value;
     public string $placeholder;
     public bool $required;
-    public string $label;
+    public bool $disabled;
 
     /**
      * Create a new component instance.
      */
     public function __construct(
+        string $label,
         string $name,
         string $id,
         string $value,
         string $placeholder,
-        bool $required,
-        string $label
+        bool $required = false,
+        bool $disabled = false
     ) {
+        $this->label = $label;
         $this->name = $name;
         $this->id = $id;
         $this->value = $value;
         $this->placeholder = $placeholder;
         $this->required = $required;
-        $this->label = $label;
+        $this->disabled = $disabled;
     }
 
     /**
--- resources/views/components/form/input-textarea.blade.php
+++ resources/views/components/form/input-textarea.blade.php
@@ -1,8 +1,10 @@
 <div>
-    <label for="{{ $id }}" class="block mb-2 text-sm font-medium text-white">
+    <label for="{{ $id }}" class="text-black dark:text-white block mb-2 text-sm font-medium">
         {{ $label }}
     </label>
     <textarea name="{{ $name }}" id="{{ $id }}"
-        class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
+        class="border sm:text-sm rounded-lg block w-full p-2.5
+    dark:bg-gray-700 dark:border-gray-600 dark:text-white
+    light:bg-gray-200 light:border-gray-300 light:text-black {{ $disabled ? 'opacity-75' : '' }}"
         value="{{ $value }}" placeholder="{{ $placeholder }}" required="{{ $required }}">{{ $value }}</textarea>
 </div>
