95d819cc1e5febaabc18d9b87ddc0761ba34409a
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-24 13:11:32 +0600

    added adjustment for dark mode

--- resources/views/components/form/input-checkbox.blade.php
+++ resources/views/components/form/input-checkbox.blade.php
@@ -1,10 +1,10 @@
 <div class="flex items-start">
     <div class="flex items-center h-5">
         <input id="{{ $id }}" name="{{ $name }}" type="checkbox"
-            class="w-4 h-4 rounded bg-gray-700 border-gray-600" checked="{{ $checked }}">
+            class="w-4 h-4 rounded dark:bg-gray-700 border-gray-600" {{ $checked ? 'checked' : '' }}>
     </div>
-    <div class="ml-3 text-sm">
-        <label for="{{ $id }}" class="font-medium text-white">
+    <div class="ml-2 text-sm">
+        <label for="{{ $id }}" class="text-black dark:text-white font-medium">
             {{ $label }}
         </label>
     </div>
