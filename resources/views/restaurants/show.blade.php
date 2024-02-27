dd2c29e67c255e66df81a39653420c2c38aa2075
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-27 22:50:08 +0600

    remove app.js as it is not necessary

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $restaurant->name }} Menu</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css'])
     @livewireStyles
 
     <style>
