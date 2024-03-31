205494c0d84b0acd70ef576c1ff7d1713e36e37b
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-03-31 20:48:00 +0600

    fix padding of the input box

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -13,7 +13,7 @@
         {{ $type == 'number' && $min != null ? 'min=' . $min : '' }}
         {{ $type == 'number' && $max != null ? 'max=' . $max : '' }}
         {{ $type == 'number' && $step != null ? 'step=' . $step : '' }}
-        class="border sm:text-sm rounded-lg block w-full p-2.5
+        class="border text-sm rounded-lg block w-full p-2
         dark:bg-gray-700 dark:border-gray-600 dark:text-white
         light:bg-gray-200 light:border-gray-300 light:text-black {{ $disabled ? 'opacity-75' : '' }}"
         {{ $valueAttr }} placeholder="{{ $placeholder }}" required="{{ $required }}"
